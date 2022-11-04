450e3a24e9160e2afd780c19367fa743f657f171
lunari <solodevlunari21@gmail.com>
2022-11-04 11:39:17 +0800

    Made textmaker a static class, removed dependency with gamescene, refactored maketext and added get/set for gamescenelength

--- src/com/example/demo/TextMaker.java
+++ src/com/example/demo/TextMaker.java
@@ -5,26 +5,35 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-class TextMaker {
-    private static TextMaker singleInstance = null;
+//Update: textmaker is now seperate of gamescene and fully static
+//I accidentally changed a significant amount of textmaker's code without setting up a test case
+//Thankfully, not much should change as most of it was setting up variables and stuff
+public class TextMaker {
+	private static double gameSceneLength;
 
-    private TextMaker() {
+	public static double getGameSceneLength() {
+		return gameSceneLength;
+	}
 
-    }
-
-    static TextMaker getSingleInstance() {
-        if (singleInstance == null)
-            singleInstance = new TextMaker();
-        return singleInstance;
-    }
-
-    Text madeText(String input, double xCell, double yCell, Group root) {
-        double length = GameScene.getLENGTH();
-        double fontSize = (3 * length) / 7.0;
+	public static void setGameSceneLength(double gameSceneLength) {
+		TextMaker.gameSceneLength = gameSceneLength;
+	}
+	
+    public static Text makeText(String input, double xCell, double yCell, Group root) {
+    	double unknownDivider = 7.0; //Not sure what it does precisely
+    	double fontScale = 3;
+    	double xScale = 1.2;
+    	double yScale = 2;
+    	
+        double fontSize = (fontScale * gameSceneLength) / unknownDivider;
+        Color whiteColor = Color.WHITE;
+        double xCellpos = xCell + xScale * gameSceneLength / unknownDivider;
+        double yCellpos = yCell + yScale * gameSceneLength / unknownDivider;
+        
         Text text = new Text(input);
         text.setFont(Font.font(fontSize));
-        text.relocate((xCell + (1.2)* length / 7.0), (yCell + 2 * length / 7.0));
-        text.setFill(Color.WHITE);
+        text.relocate(xCellpos, yCellpos);
+        text.setFill(whiteColor);
 
         return text;
     }
