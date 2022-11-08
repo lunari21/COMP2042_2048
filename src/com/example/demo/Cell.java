e811688dc7186c3b3a6ecb6bf19c90e6162dc546
lunari <solodevlunari21@gmail.com>
2022-11-08 18:05:09 +0800

    Updated a significant amount of the Cell class. Refer to comments for updates

--- src/com/example/demo/Cell.java
+++ src/com/example/demo/Cell.java
@@ -8,15 +8,71 @@ import javafx.scene.text.Text;
 /**
  * Cell class that represents individual cells in the game
  * @author Unknown, Refactoring by Alexander Tan Ka Jin
- *
+ * Doc will be added later.
  */
 //Update: cell is composite of GameScene
 public class Cell {
     private Rectangle rectangle;
     private Group root;
+    //the text displayed on the cell
     private Text textClass;
+    //value of the cell, new functionality to make it less of a headache
+    private int number;
     private boolean modify = false;
 
+    /**
+     * Constructor for cell objects
+     * @param x - The x position of the rectangle when displayed
+     * @param y - The y position of the rectangle when displayed
+     * @param scale - The size of the rectangle when displayed
+     * @param root - The root group for this cell's rectangle
+     */
+    public Cell(double x, double y, double scale, Group root, double SceneLength) {
+    	Color startingColor = Color.rgb(224, 226, 226, 0.5);
+    	//Might wanna make a class holding all colors
+    	
+    	//Set up a couple variables
+        this.rectangle = new Rectangle();
+        this.rectangle.setX(x);
+        this.rectangle.setY(y);
+        this.rectangle.setHeight(scale);
+        this.rectangle.setWidth(scale);
+        //Initially it's value is 0
+        this.number = 0;
+        this.textClass = TextMaker.formatText(String.valueOf(number), x, y, SceneLength);
+        this.root = root;
+        this.rectangle.setFill(startingColor);
+        
+        //Add this object to root
+        root.getChildren().add(this.rectangle);
+    }
+    
+    public double getX() {
+        return rectangle.getX();
+    }
+
+    public double getY() {
+        return rectangle.getY();
+    }
+
+    @Deprecated //Use UpdateText instead
+    public void setTextClass(Text txt) {
+    	this.textClass = txt;
+    }
+
+    //Changed to public. Don't really understand why it was private
+    public Text getTextClass() {
+        return textClass;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public void setNumber(int val) {
+    	this.number = val;
+    }
+    
     public void setModify(boolean modify) {
         this.modify = modify;
     }
@@ -24,50 +80,43 @@ public class Cell {
     public boolean getModify() {
         return modify;
     }
-
-    /**
-     * Constructor for cell objects
-     * @param x - The x position when displayed
-     * @param y - The y position when displayed
-     * @param scale - The size of the cell when displayed
-     * @param root - The root group for this cell
-     */
-    public Cell(double x, double y, double scale, Group root) {
-    	Color startingColor = Color.rgb(224, 226, 226, 0.5);
-    	//Might wanna make a class holding all colors
-    	
-        this.rectangle = new Rectangle();
-        this.rectangle.setX(x);
-        this.rectangle.setY(y);
-        this.rectangle.setHeight(scale);
-        this.rectangle.setWidth(scale);
-        this.textClass = TextMaker.formatText("0", x, y);
-        this.root = root;
-        this.rectangle.setFill(startingColor);
-        
-        //Add this object to root
-        root.getChildren().add(this.rectangle);
+    
+    public void swapValue(Cell cell) {
+    	int temp = number;
+    	//If number is swapped, so is the text of textclass
+    	this.number = cell.getNumber();
+    	cell.setNumber(temp);
     }
 
-    //Changed changeCell to MergeCell
-    public void mergeCell(Cell cell) {
-        TextMaker.SwapText(textClass, cell.getTextClass());
+    public void SwapWith (Cell cell) {
+    	//Swap textclass positions
         TextMaker.SwapPos(textClass, cell.getTextClass());
-        
+        //Swap value
+        swapValue(cell);
+        //swap Text
+        TextMaker.SwapText(textClass, cell.getTextClass());
+    }
+    
+    @Deprecated //Use SwapWith then update cell grid to remove zeros instead
+    public void changeCell(Cell cell) {
+    	SwapWith(cell);
+        //This might go deprecated and be changed to a functionality of gamescene instead
+        //Remove first. Javafx is unkind to duplicate children.
         root.getChildren().remove(cell.getTextClass());
-        //Wait was textClass supposed to be in root when made?
         root.getChildren().remove(textClass);
         
-        if (!cell.getTextClass().getText().equals("0")) {
-            root.getChildren().add(cell.getTextClass());
-        }
-        if (!textClass.getText().equals("0")) {
-            root.getChildren().add(textClass);
-        }
-        setColorByNumber(getNumber());
-        cell.setColorByNumber(cell.getNumber());
+        //If one or both of them is 0, then don't add it back
+        if (cell.getNumber() != 0)
+        	root.getChildren().add(cell.getTextClass());
+          
+        if (getNumber() != 0)
+        	root.getChildren().add(textClass);
+            
+        cell.UpdateColor();
+        UpdateColor();
     }
 
+    @Deprecated //Use mergeWith instead
     public void adder(Cell cell) {
         cell.getTextClass().setText((cell.getNumber() + this.getNumber()) + "");
         textClass.setText("0");
@@ -75,7 +124,16 @@ public class Cell {
         cell.setColorByNumber(cell.getNumber());
         setColorByNumber(getNumber());
     }
-
+    
+    //improved version
+    public void mergeWith(Cell cell) {
+        cell.setNumber(number+cell.getNumber());
+        setNumber(0);
+        root.getChildren().remove(textClass);
+        cell.UpdateColor();
+        UpdateColor();
+    }
+    //Unsure how to best clean this up atm
     public void setColorByNumber(int number) {
         switch (number) {
             case 0:
@@ -113,30 +171,14 @@ public class Cell {
                 break;
             case 2048:
                 rectangle.setFill(Color.rgb(250,0,0,1));
-
-
         }
-
-    }
-
-    public double getX() {
-        return rectangle.getX();
-    }
-
-    public double getY() {
-        return rectangle.getY();
-    }
-
-    public int getNumber() {
-        return Integer.parseInt(textClass.getText());
-    }
-
-    private Text getTextClass() {
-        return textClass;
     }
     
-    public void setTextClass(Text textClass) {
-        this.textClass = textClass;
+    public void UpdateColor() {
+    	setColorByNumber(number);
     }
-
+    
+    public void UpdateText() {
+    	this.textClass.setText(String.valueOf(number));
+    }
 }
