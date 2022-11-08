0fda08c546678e93281c6416276183f9cf8c6d35
lunari <solodevlunari21@gmail.com>
2022-11-08 14:35:42 +0800

    Textmaker is now a util class.

--- src/com/example/demo/TextMaker.java
+++ src/com/example/demo/TextMaker.java
@@ -1,35 +1,15 @@
 package com.example.demo;
 
-import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
- * A static class that hosts several tools on manipulating text
+ * A class that hosts several tools on manipulating text for gamescene.
  * @author Unknown, refactoring by Alexander Tan Ka Jin
  * @version 2
  */
 public class TextMaker {
-	private static double gameSceneLength; //This needs to be removed I think
-	//Since if some rogue function comes in and changes this, it might screw up a lot of stuff
-
-	/**
-	 * Gets the length of the game scene
-	 * @return length of the game scene
-	 */
-	public static double getGameSceneLength() {
-		return gameSceneLength;
-	}
-
-	/**
-	 * Updates the length of the game scene for this class
-	 * @param gameSceneLength - double
-	 */
-	public static void setGameSceneLength(double gameSceneLength) {
-		TextMaker.gameSceneLength = gameSceneLength;
-	}
-	
 	/**
 	 * Makes text in accordance with the format of the game. Docs is somewhat unfinished
 	 * @param input - the text to be displayed
@@ -37,17 +17,18 @@ public class TextMaker {
 	 * @param yCell - ??
 	 * @return text object with white font
 	 */
-    public static Text formatText(String input, double xCell, double yCell) {
+    public static Text formatText(String input, double xCell, double yCell, double sceneLength) {
     	double unknownDivider = 7.0; //Not sure what it does precisely
     	double fontScale = 3;
     	double xScale = 1.2;
     	double yScale = 2;
     	
-        double fontSize = (fontScale * gameSceneLength) / unknownDivider;
+        double fontSize = (fontScale * sceneLength) / unknownDivider;
         Color whiteColor = Color.WHITE;
-        double xCellpos = xCell + xScale * gameSceneLength / unknownDivider;
-        double yCellpos = yCell + yScale * gameSceneLength / unknownDivider;
+        double xCellpos = xCell + xScale * sceneLength / unknownDivider;
+        double yCellpos = yCell + yScale * sceneLength / unknownDivider;
         
+        //Create text
         Text text = new Text(input);
         text.setFont(Font.font(fontSize));
         text.relocate(xCellpos, yCellpos);
