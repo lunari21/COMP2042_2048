ec319f5d73717fee72495c5bbdc63bafa1dcc5d1
Lunari <solodevlunari21@gmail.com>
2022-12-13 18:29:21 +0800

    Added docs for movedirection

--- src/main/model/MoveDirection.java
+++ src/main/model/MoveDirection.java
@@ -1,8 +1,13 @@
 package main.model;
 
+/**
+ * Enum for dictating movement directions in GameModel.
+ * 
+ * @author Alexander Tan Ka Jin
+ */
 public enum MoveDirection {
 	LEFT,
 	RIGHT,
 	UP,
 	DOWN
-	}
+}
