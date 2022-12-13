c18000e48681494bc082cc4743aa4a586d8c314c
Lunari <solodevlunari21@gmail.com>
2022-12-13 17:56:56 +0800

    Updated docs for CellGrid

--- src/main/model/CellGrid.java
+++ src/main/model/CellGrid.java
@@ -22,10 +22,8 @@ public class CellGrid {
 
 	/**
 	 * Constructor for cell grid
-	 * @param gridSize - Size of this square grid
-	 * @param sceneLength - Length of the game scene
-	 * @param cellDistance - Distance between each cells
-	 * @param root - The game root.
+	 * @param gridScaleX - the width of the grid
+	 * @param gridScaleY - the height of the grid
 	 */
 	public CellGrid(int gridScaleX,int gridScaleY) {
 		this.grid = new int[gridScaleY][gridScaleX];
@@ -36,14 +34,21 @@ public class CellGrid {
 	/**
 	 * Retrieves cell at position (x,y) between (0,0) to (n-1,n-1), n being the
 	 * Length of the grid.
-	 * @param x
-	 * @param y
+	 * @param x - x position of the cell
+	 * @param y - y position of the cell
 	 * @return The specific cell at x and y
 	 */
 	public int getCell(int x, int y) {
 		return grid[y][x];
 	}
 
+	/**
+	 * Sets the value of a cell on coordinates x and y
+	 * @param x - x position of the cell
+	 * @param y - y position of the cell
+	 * @param val - New value of the cell
+	 * @return Returns this object.
+	 */
 	public CellGrid setCell(int x, int y, int val) {
 		grid[y][x] = val;
 		return this;
@@ -51,22 +56,22 @@ public class CellGrid {
 
 	/**
 	 * Returns the whole grid, mainly used for testing and display.
-	 * @return int[][]
+	 * @return int[][] - The grid represented as a 2d array of integers
 	 */
 	public int[][] getGrid(){
 		return grid;
 	}
 
 	/**
-	 * Sets integer grid g to this object's grid.
-	 * @param g - int[][]
-	 * @return regardless of whether g is compatible, returns this object.
+	 * Sets a 2d integer grid as this object's grid.
+	 * @param newGrid - The new value of this object's 2d array
+	 * @return Regardless of whether newGrid is compatible, returns this object.
 	 */
-	public CellGrid setGrid(int[][] g) {
+	public CellGrid setGrid(int[][] newGrid) {
 		try {
 			for (int y = 0; y <= DOWNCELLS; y++) {
 				for (int x = 0; x <= RIGHTCELLS; x++) {
-					setCell(x, y, g[y][x]);
+					setCell(x, y, newGrid[y][x]);
 				}
 			}
 			return this;
@@ -78,7 +83,7 @@ public class CellGrid {
 
 	/**
 	 * Gets the left edge of the grid
-	 * @return square grid length
+	 * @return The first index of the grid when going from left to right.
 	 */
 	public int getLeftEdge() {
 		return LEFTCELLS;
@@ -86,7 +91,7 @@ public class CellGrid {
 
 	/**
 	 * Gets the right edge of the grid
-	 * @return square grid length
+	 * @return The last index of the grid when going from left to right.
 	 */
 	public int getRightEdge() {
 		return RIGHTCELLS;
@@ -94,7 +99,7 @@ public class CellGrid {
 
 	/**
 	 * Gets the bottom edge of the grid
-	 * @return square grid length
+	 * @return The last index of the grid when going from top to bottom.
 	 */
 	public int getDownEdge() {
 		return DOWNCELLS;
@@ -102,12 +107,17 @@ public class CellGrid {
 
 	/**
 	 * Gets the top edge of the grid
-	 * @return square grid length
+	 * @return The first index of the grid when going from top to bottom.
 	 */
 	public int getUpEdge() {
 		return UPCELLS;
 	}
 
+	/**
+	 * Gets a specified edge based on the direction dir.
+	 * @param dir - The direction of the edge (LEFT, RIGHT, UP, DOWN)
+	 * @return The value of the edge based on dir.
+	 */
 	public int getEdge(MoveDirection dir) {
 		switch (dir) {
 		case LEFT:
@@ -123,9 +133,9 @@ public class CellGrid {
 	}
 
 	/**
-	 * If the grid has a specific value, returns true, else false
-	 * @param val - int
-	 * @return boolean
+	 * Checks if the grid has a specified val.
+	 * @param val - the value that is going to be checked.
+	 * @return Whether the value is present in the grid.
 	 */
 	public boolean contains(int val) {
 		for (int y = 0; y <= DOWNCELLS; y++) {
@@ -138,14 +148,18 @@ public class CellGrid {
 	}
 
 	/**
-	 * Checks if two cellgrids are the same
-	 * @param prevState - previous state
-	 * @return true if the board state was changed, false if not
+	 * Checks if two cellGrids are the same
+	 * @param b - A second cellGrid to be compared.
+	 * @return True if it is equivalent. False if not.
 	 */
 	public boolean gridEquals (CellGrid b) {
 		return Arrays.deepEquals(grid, b.getGrid());
 	}
 
+	/**
+	 * Override from java.lang.Object.
+	 * @return A copy of this object.
+	 */
 	@Override
 	public CellGrid clone (){
 		CellGrid out = new CellGrid(RIGHTCELLS+1,DOWNCELLS+1);
